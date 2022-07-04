d9b51f2f872584e7ddcf71864d00228f25c02817
mmrichmond6 <mmrichmond6@gmail.com>
2022-07-04 13:09:02 -0500

    Updated Return statements

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // import { validateInput } from './scriptHelper';
 
 window.addEventListener("load", function() {
+
+    let pilot = document.querySelector("input[name=pilotName]");
+    let copilot = document.querySelector("input[name=copilotName]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoMass = document.querySelector("input[name=cargoMass]");
+
+    let pilotName = pilot.value;
+    let copilotName = copilot.value;
+    let fuelLevelNum = fuelLevel.value;
+    let cargoMassNum = cargoMass.value;
+    
     let form = document.querySelector("form");
     form.addEventListener("submit", function(event) {
     let pilot = document.querySelector("input[name=pilotName]");
@@ -12,51 +23,36 @@ window.addEventListener("load", function() {
     let copilotName = copilot.value;
     let fuelLevelNum = fuelLevel.value;
     let cargoMassNum = cargoMass.value;
-
                 
-        if (pilot.value === "" || copilot.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+    if (pilot.value === "" || copilot.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
         alert("All fields are required!");
         event.preventDefault();
-        } else if (!isNaN(pilotName)) {
-            alert(`Pilot name must be a string! ${pilotName}`);
-            event.preventDefault();
-        } else if (!isNaN(copilotName)) {
-            alert(`Co-pilot name must be a string! ${copilotName}`);
-            event.preventDefault();
-        } else if (isNaN(fuelLevelNum)) {
-            alert(`Fuel Level (L) must be a number! ${fuelLevelNum}`);
-            event.preventDefault();
-        } else if (isNaN(cargoMassNum)) {
-            alert(`Cargo Mass (kg) must be a number! ${cargoMassNum}`);
-            event.preventDefault();
-        } else {
-            alert(`You are submitting the following information:  Pilot Name: ${pilotName}; Co-pilot Name: ${copilotName}; Fuel Level (L): ${fuelLevelNum}; Cargo Mass (kg): ${cargoMassNum}`)
-        }
+        return console.log("Empty");
+    } else if (!isNaN(pilotName)) {
+        alert(`Pilot name must be a string! ${pilotName}`);
+        event.preventDefault();
+        return console.log("Is a number");
+    } else if (!isNaN(copilotName)) {
+        alert(`Co-pilot name must be a string! ${copilotName}`);
+        event.preventDefault();
+        return console.log("Is a number");
+    } else if (isNaN(fuelLevelNum)) {
+        alert(`Fuel Level (L) must be a number! ${fuelLevelNum}`);
+        event.preventDefault();
+        return console.log("Not a number");
+    } else if (isNaN(cargoMassNum)) {
+        alert(`Cargo Mass (kg) must be a number! ${cargoMassNum}`);
+        event.preventDefault();
+        return console.log("Not a number");
+    } else {
+        alert(`You are submitting the following information:  Pilot Name: ${pilotName}; Co-pilot Name: ${copilotName}; Fuel Level (L): ${fuelLevelNum}; Cargo Mass (kg): ${cargoMassNum}`);
+        return console.log(`Pilot Name: ${pilotName}; Co-pilot Name: ${copilotName}; Fuel Level (L): ${fuelLevelNum}; Cargo Mass (kg): ${cargoMassNum}`);
+    }
 
     });
-       
-        
-        // if (Number.isNaN(pilot.value)) {
-        //     alert(pilot.value + " must be a string!");
-        //     event.preventDefault();
-        // }
 
-        // if (Number.isNaN(copilot.value)) {
-        //     alert(pilot.value + " must be a string!");
-        //     event.preventDefault();
-        // }
-
-        // if (Number.isNaN(fuelLevel.value)) {
-        //     alert(pilot.value + " must be a string!");
-        //     event.preventDefault();
-        // }
-
-        // if (Number.isNaN(cargoLevel.value)) {
-        //     alert(pilot.value + " must be a string!");
-        //     event.preventDefault();
-    //     }
-    // })
-
+    
+     
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
